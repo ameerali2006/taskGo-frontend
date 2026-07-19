6432d778fe5a2ec2cbfc73a0885489e2ac77cc1d
Ameer Ali <ameerali10b@gmail.com>
2026-07-19 16:18:05 +0530

    feat: initialize authenticated userAxios instance with token refresh and logout logic

--- src/config/axios/userAxios.ts
+++ src/config/axios/userAxios.ts
@@ -1,11 +1,12 @@
 import { createAxiosClient } from "./createAxiosClient";
 import { store } from "../../redux/store";
 import { removeUser } from "../../redux/authSlice";
-import { env } from "../../env/env";
 import { apiRoutes } from "../../constants/apiRoutes";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
 export const userAxios = createAxiosClient({
-  baseURL: env.API_BASE_URL,
+  baseURL: API_BASE_URL,
   refreshTokenEndpoint: apiRoutes.auth.refreshToken,
   loginRedirect: () => {
     window.location.href = "/login";
